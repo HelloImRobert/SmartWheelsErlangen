155dd0a5fa9769d64ec7b1fae348a5889ff39746
car17 <mscheuerer@gmx.de>
2015-02-23 17:06:45 +0100

    Commit BirdEye (Markus)

---
+++ src/SWE_BirdEyeTransformation/stdafx.h
@@ -0,0 +1,17 @@
+#ifndef __STD_INCLUDES_HEADER
+#define __STD_INCLUDES_HEADER
+
+#include <adtf_platform_inc.h> // necessary for OpenCV !
+#include <adtf_plugin_sdk.h>
+using namespace adtf;
+
+// opencv header
+#include <opencv/cv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ f�gt zus�tzliche Deklarationen unmittelbar vor der vorherigen Zeile ein.
+
+#endif // __STD_INCLUDES_HEADER
