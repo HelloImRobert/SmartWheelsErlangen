8ebd3d32e1485a9c6bd268cf11c9432fb26dd212
car02 <robert.detemple@gmail.com>
2015-02-28 17:50:55 +0100

    ParkPilot sollte jetzt Parkluecke finden (Markus)

--- src/SWE_ParkPilot/SWE_ParkPilot.cpp
+++ src/SWE_ParkPilot/SWE_ParkPilot.cpp
@@ -1,154 +1,334 @@
-//#include <cmath>
-//#include "stdafx.h"
-//#include "SWE_ParkPilot.h"
+#include <cmath>
+#include "stdafx.h"
+#include "SWE_ParkPilot.h"
 
-//#include <iostream>
-//#include <fstream>
+#include <iostream>
+#include <fstream>
 
-//#define POS_IR_SIDE_RIGHT -150.0      //y-Value
+// +++ begin_defines +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#define POS_IR_SIDE_RIGHT -150.0    //y-Value of IR sensors
+#define TH_SHORT 140.0
+#define TH_LONG_ALONGSIDE 590.0
+#define TH_LONG_CROSS 590.0
 
-//ADTF_FILTER_PLUGIN("SWE ParkPilot", OID_ADTF_SWE_PARKPILOT, cSWE_ParkPilot)
+#define PARK_ALONGSIDE 1            // park style
+#define PARK_CROSS 2                // park style
 
-//cSWE_ParkPilot::cSWE_ParkPilot(const tChar* __info) : cFilter(__info)
-//{
+#define ALONGSIDE_SIZE 900          // minimum size of parking lot
+#define CROSS_SIZE 500              // minimum size of parking lot
+#define EASY_ALONGSIDE 100          // buffer for easy S-curve maneuver
 
+#define INVALIDE_LOW 0.0
+#define INVALIDE_HIGH 9999.0
 
-//}
-
-//cSWE_ParkPilot::~cSWE_ParkPilot()
-//{
-//}
-
-//tResult cSWE_ParkPilot::CreateInputPins(__exception)
-//{
-//    RETURN_IF_FAILED(m_inputParkTrigger.Create("Park Trigger", new cMediaType(0, 0, 0, "tSBool"), static_cast<IPinEventSink*> (this)));
-//    RETURN_IF_FAILED(RegisterPin(&m_inputParkTrigger));
-//    RETURN_NOERROR;
-//}
-
-//tResult cSWE_ParkPilot::CreateOutputPins(__exception)
-//{
-////    cObjectPtr<IMediaDescriptionManager> pDescManager;
-////    RETURN_IF_FAILED(_runtime->GetObject(OID_ADTF_MEDIA_DESCRIPTION_MANAGER,IID_ADTF_MEDIA_DESCRIPTION_MANAGER,(tVoid**)&pDescManager,__exception_ptr));
+#define POS_IR_FRONT_SIDE 450.0     // x-Pos of front IR sensor
 
 
-
-////    // Left Intersection Point
-////    // TO ADAPT for new Pin/Dadatype: strDescPointLeft, "tPoint2d", pTypePointLeft, m_pCoderDescPointLeft, m_oIntersectionPointLeft, "left_Intersection_Point" !!!!!!!!!!!!!!!!!!!!
-////    tChar const * strDescPointLeft = pDescManager->GetMediaDescription("tPoint2d");
-////    RETURN_IF_POINTER_NULL(strDescPointLeft);
-////    cObjectPtr<IMediaType> pTypePointLeft = new cMediaType(0, 0, 0, "tPoint2d", strDescPointLeft,IMediaDescription::MDF_DDL_DEFAULT_VERSION);
-////    RETURN_IF_FAILED(pTypePointLeft->GetInterface(IID_ADTF_MEDIA_TYPE_DESCRIPTION, (tVoid**)&m_pCoderDescPointLeft));
-
-////    RETURN_IF_FAILED(m_oIntersectionPointLeft.Create("left_Intersection_Point", pTypePointLeft, static_cast<IPinEventSink*> (this)));
-////    RETURN_IF_FAILED(RegisterPin(&m_oIntersectionPointLeft));
+// +++ end_defines ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-//      RETURN_NOERROR;
-//}
+ADTF_FILTER_PLUGIN("SWE ParkPilot", OID_ADTF_SWE_PARKPILOT, cSWE_ParkPilot)
 
-//tResult cSWE_ParkPilot::Init(tInitStage eStage, __exception)
-//{
-//    RETURN_IF_FAILED(cFilter::Init(eStage, __exception_ptr))
+cSWE_ParkPilot::cSWE_ParkPilot(const tChar* __info) : cFilter(__info)
+{
+        m_IRFrontRightCur = INVALIDE_HIGH;
+        m_IRRearRightCur = INVALIDE_HIGH;
+        m_IRFrontRightOld = INVALIDE_HIGH;
+        m_IRRearRightOld = INVALIDE_HIGH;
+        m_distCur = 0.0;
+        m_distEntry = 0.0;
+        m_parkTrigger = 0;
+        m_lastIRshort = 0.0;
+        m_searchActive = false;
+        m_entry = false;
+        m_entrySaved = false;
+        m_minDistReached = false;
+        m_odometryData.angle = 0.0;
+        m_odometryData.distance = 0.0;
+        m_odometryData.distanceSum = 0.0;
+        m_odometryData.heading = 0.0;
 
-//    if (eStage == StageFirst)
+}
+
+cSWE_ParkPilot::~cSWE_ParkPilot()
+{
+}
+
+tResult cSWE_ParkPilot::CreateInputPins(__exception)
+{
+    RETURN_IF_FAILED(m_inputParkTrigger.Create("Park Trigger", new cMediaType(0, 0, 0, "tInt"), static_cast<IPinEventSink*> (this)));
+    RETURN_IF_FAILED(RegisterPin(&m_inputParkTrigger));
+    RETURN_NOERROR;
+}
+
+tResult cSWE_ParkPilot::CreateOutputPins(__exception)
+{
+//    cObjectPtr<IMediaDescriptionManager> pDescManager;
+//    RETURN_IF_FAILED(_runtime->GetObject(OID_ADTF_MEDIA_DESCRIPTION_MANAGER,IID_ADTF_MEDIA_DESCRIPTION_MANAGER,(tVoid**)&pDescManager,__exception_ptr));
+
+
+
+//    // Left Intersection Point
+//    // TO ADAPT for new Pin/Dadatype: strDescPointLeft, "tPoint2d", pTypePointLeft, m_pCoderDescPointLeft, m_oIntersectionPointLeft, "left_Intersection_Point" !!!!!!!!!!!!!!!!!!!!
+//    tChar const * strDescPointLeft = pDescManager->GetMediaDescription("tPoint2d");
+//    RETURN_IF_POINTER_NULL(strDescPointLeft);
+//    cObjectPtr<IMediaType> pTypePointLeft = new cMediaType(0, 0, 0, "tPoint2d", strDescPointLeft,IMediaDescription::MDF_DDL_DEFAULT_VERSION);
+//    RETURN_IF_FAILED(pTypePointLeft->GetInterface(IID_ADTF_MEDIA_TYPE_DESCRIPTION, (tVoid**)&m_pCoderDescPointLeft));
+
+//    RETURN_IF_FAILED(m_oIntersectionPointLeft.Create("left_Intersection_Point", pTypePointLeft, static_cast<IPinEventSink*> (this)));
+//    RETURN_IF_FAILED(RegisterPin(&m_oIntersectionPointLeft));
+
+
+      RETURN_NOERROR;
+}
+
+tResult cSWE_ParkPilot::Init(tInitStage eStage, __exception)
+{
+    RETURN_IF_FAILED(cFilter::Init(eStage, __exception_ptr))
+
+    if (eStage == StageFirst)
+    {
+        CreateInputPins(__exception_ptr);
+        CreateOutputPins(__exception_ptr);
+    }
+    else if (eStage == StageNormal)
+    {
+
+    }
+    else if(eStage == StageGraphReady)
+    {
+
+    }
+
+    RETURN_NOERROR;
+}
+
+tResult cSWE_ParkPilot::Start(__exception)
+{
+    return cFilter::Start(__exception_ptr);
+}
+
+tResult cSWE_ParkPilot::Stop(__exception)
+{
+    return cFilter::Stop(__exception_ptr);
+}
+
+tResult cSWE_ParkPilot::Shutdown(tInitStage eStage, __exception)
+{
+    return cFilter::Shutdown(eStage,__exception_ptr);
+}
+
+tResult cSWE_ParkPilot::OnPinEvent(	IPin* pSource, tInt nEventCode, tInt nParam1, tInt nParam2, IMediaSample* pMediaSample)
+{
+    cObjectPtr<IMediaType> pType;
+    pSource->GetMediaType(&pType);
+
+
+//    if (pType != NULL)
 //    {
-//        CreateInputPins(__exception_ptr);
-//        CreateOutputPins(__exception_ptr);
-//    }
-//    else if (eStage == StageNormal)
-//    {
-
-//    }
-//    else if(eStage == StageGraphReady)
-//    {
+//        cObjectPtr<IMediaTypeDescription> pMediaTypeDescParkBool;
+//        RETURN_IF_FAILED(pType->GetInterface(IID_ADTF_MEDIA_TYPE_DESCRIPTION, (tVoid**)&pMediaTypeDescParkBool));
+//        m_pCoderDescParkBool = pMediaTypeDescParkBool;
 
 //    }
 
-//    RETURN_NOERROR;
-//}
+    if (nEventCode == IPinEventSink::PE_MediaSampleReceived && pMediaSample != NULL && m_pCoderDesc != NULL)
+    {
 
-//tResult cSWE_ParkPilot::Start(__exception)
-//{
-//    return cFilter::Start(__exception_ptr);
-//}
+        RETURN_IF_POINTER_NULL( pMediaSample);
 
-//tResult cSWE_ParkPilot::Stop(__exception)
-//{
-//    return cFilter::Stop(__exception_ptr);
-//}
+        if(pSource == &m_inputParkTrigger)
+        {
+           // save the park trigger: alongside or cross
+           cObjectPtr<IMediaCoder> pCoder;
+           RETURN_IF_FAILED(m_pCoderDesc->Lock(pMediaSample, &pCoder));
+           pCoder->Get("tInt", (tVoid*)&m_parkTrigger);
+           m_pCoderDesc->Unlock(pCoder);
 
-//tResult cSWE_ParkPilot::Shutdown(tInitStage eStage, __exception)
-//{
-//    return cFilter::Shutdown(eStage,__exception_ptr);
-//}
+        }
+        else if(pSource == &m_ObjectData)
+        {
+            // leave if we have not received a park trigger
+            if( m_parkTrigger != PARK_ALONGSIDE && m_parkTrigger != PARK_CROSS )
+            {
+                RETURN_NOERROR;
+            }
 
-//tResult cSWE_ParkPilot::OnPinEvent(	IPin* pSource, tInt nEventCode, tInt nParam1, tInt nParam2, IMediaSample* pMediaSample)
-//{
-//    cObjectPtr<IMediaType> pType;
-//    pSource->GetMediaType(&pType);
+            // if we have received a park trigger: take a look at the IR data
+            cv::Point2d objectData[10];
+            cObjectPtr<IMediaCoder> pCoder;
+            RETURN_IF_FAILED(m_pCoderDesc->Lock(pMediaSample, &pCoder));
+            pCoder->Get("PointArray", (tVoid*)&objectData);
+            m_pCoderDesc->Unlock(pCoder);
 
+            m_IRFrontRightCur = (-1) * (objectData[1].y - POS_IR_SIDE_RIGHT);
+            m_IRRearRightCur = (-1) * (objectData[4].y - POS_IR_SIDE_RIGHT);
 
-////    if (pType != NULL)
-////    {
-////        cObjectPtr<IMediaTypeDescription> pMediaTypeDescParkBool;
-////        RETURN_IF_FAILED(pType->GetInterface(IID_ADTF_MEDIA_TYPE_DESCRIPTION, (tVoid**)&pMediaTypeDescParkBool));
-////        m_pCoderDescParkBool = pMediaTypeDescParkBool;
+            // save latest "short" value
+            if( m_IRFrontRightCur < TH_SHORT )
+            {
+                m_lastIRshort = m_IRFrontRightCur;
+            }
 
-////    }
+            // Check whether we have reached the first car; if yes, activate the search
+            if(m_IRFrontRightCur > TH_SHORT && m_searchActive == false)
+            {
+                RETURN_NOERROR;
+            }
+            else
+            {
+                m_searchActive = true;
 
-//    if (nEventCode == IPinEventSink::PE_MediaSampleReceived && pMediaSample != NULL && m_pCoderDesc != NULL)
-//    {
+                if(m_parkTrigger == PARK_ALONGSIDE)
+                {
+                    searchRoutineAlongside();
+                }
+                else if(m_parkTrigger == PARK_CROSS)
+                {
+                    searchRoutineCross();
+                }
+                else
+                {
+                    RETURN_NOERROR;
+                }
+            }
 
-//        RETURN_IF_POINTER_NULL( pMediaSample);
-
-//        if(pSource == &m_inputParkTrigger)
-//        {
-//           cObjectPtr<IMediaCoder> pCoder;
-//           RETURN_IF_FAILED(m_pCoderDesc->Lock(pMediaSample, &pCoder));
-//           tBool goPark = false;
-//           pCoder->Get("tBool", (tVoid*)&goPark);
-//           m_pCoderDesc->Unlock(pCoder);
-
-//           if( goPark == true )
-//           {
-//               searchLot();
-//           }
-
-//        }
-//        else if(pSource == &m_ObjectData)
-//        {
-//            cv::Point2d objectData[10];
-//            cObjectPtr<IMediaCoder> pCoder;
-//            RETURN_IF_FAILED(m_pCoderDesc->Lock(pMediaSample, &pCoder));
-//            pCoder->Get("tBool", (tVoid*)&objectData);
-//            m_pCoderDesc->Unlock(pCoder);
-
-//            m_IRFrontRight = (-1) * (objectData[1].y - POS_IR_SIDE_RIGHT);
-//            m_IRRearRight = (-1) * (objectData[4].y - POS_IR_SIDE_RIGHT);
-//        }
-
-
-
-//    }
-//    RETURN_NOERROR;
-//}
-
-//tResult cSWE_ParkPilot::searchLot()
-//{
-
-//    // Go forward
-//    // Evaluate IR Front Left trough thresholding
+        }
 
 
-//    RETURN_NOERROR;
-//}
+
+    }
+    RETURN_NOERROR;
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++ ALONGSIDE ROUTINE +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+tResult cSWE_ParkPilot::searchRoutineAlongside()
+{
+    tFloat32 distTravelled = 0;
+    tFloat32 distStartPark = 0;
+    //...now search for possible lot beginning and save the distance at this point
+    if( m_IRFrontRightCur >= TH_LONG_ALONGSIDE )
+    {
+        if(m_entrySaved == false)
+        {
+            m_entry = true;
+        }
+        else
+        {
+            m_entry = false;
+        }
+
+        if(m_entry == true)
+        {
+            // save data at entry point
+            m_distEntry = m_odometryData.distance;
+            m_entrySaved = true;
+        }
+
+        if(m_entrySaved == true)
+        {
+            distTravelled = m_distCur - m_distEntry;
+
+            // check if space is already sufficient
+            if( distTravelled > ALONGSIDE_SIZE )
+            {
+                m_minDistReached = true;
+            }
+            else
+            {
+                m_minDistReached = false;
+            }
+        }
+    }
+    else if( m_IRRearRightCur < TH_SHORT && m_entrySaved == true )
+    {
+        m_entrySaved = false;
+        RETURN_NOERROR;
+    }
+
+    // minimum distance is reached....
+    // .....and we reached easy parking situation
+    if( m_minDistReached == true && distTravelled > (ALONGSIDE_SIZE + EASY_ALONGSIDE) )
+    {
+        //easy
+        distStartPark = m_distCur;
+        parkRoutineAlongside(distTravelled, distStartPark);
+    }
+    // ....and we take as much space as we can get
+    else if( m_minDistReached == true && m_IRFrontRightCur < TH_SHORT )
+    {
+        //normal
+        distStartPark = m_distCur;
+        parkRoutineAlongside(distTravelled, distStartPark);
+    }
+
+    RETURN_NOERROR;
+}
 
 
-///*
-//computes intersetion points and indicator for steering angle calculation
-//indicator values:
-//1: two intersection points found
-//0: less than two intersection points found
-//else: computation error
-//*/
+tResult cSWE_ParkPilot::parkRoutineAlongside(tFloat32 lotSize, tFloat32 distStartPark)
+{
+
+    tFloat32 centralAngle = atan( 1 + ( (150 + m_lastIRshort) / (-949) ) );
+    tFloat32 distToGo = POS_IR_FRONT_SIDE + ( 800 * sin( centralAngle ) );
+
+    if( m_distCur - distStartPark >= distToGo)
+    {
+        //VOLLBREMSUNG
+
+        //MAXIMALER LENKEINSCHLAG RECHTS
+
+        //RÜCKWÄRTS BIS ROT = centralAngle + ausgangswinkel
+
+        //MAXIMALER LENKEINLAG LINKS
+
+        //RÜCKWÄRTS BIS ROT = ausgangswinkel
+
+
+    }
+
+
+    //easy maneuver
+    if( lotSize > (ALONGSIDE_SIZE + EASY_ALONGSIDE) )
+    {
+
+    }
+    //normal maneuver
+    else
+    {
+
+    }
+
+
+    // at the end....
+    m_entry = false;
+    m_entrySaved = false;
+    m_minDistReached = false;
+    RETURN_NOERROR;
+}
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++ CROSS ROUTINE +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+tResult cSWE_ParkPilot::searchRoutineCross()
+{
+    RETURN_NOERROR;
+}
+
+tResult cSWE_ParkPilot::parkRoutineCross(tFloat32 lotSize, tFloat32 distStartPark)
+{
+    RETURN_NOERROR;
+}
+
+
+
--- src/SWE_ParkPilot/SWE_ParkPilot.h
+++ src/SWE_ParkPilot/SWE_ParkPilot.h
@@ -1,55 +1,86 @@
-//#ifndef _SWE_PARKPILOT_H_
-//#define _SWE_PARKPILOT_H_
+#ifndef _SWE_PARKPILOT_H_
+#define _SWE_PARKPILOT_H_
 
-//#include "stdafx.h"
-
-
-//#define OID_ADTF_SWE_PARKPILOT "adtf.swe.parkpilot"
-
-///*!
-//* Intersection Point Calculator
-//*/
-//class cSWE_ParkPilot : public adtf::cFilter
-//{
-//    ADTF_DECLARE_FILTER_VERSION(OID_ADTF_SWE_PARKPILOT, "SWE ParkPilot", OBJCAT_DataFilter, "ParkPilot", 1, 0,0, "pre alpha version");
-
-//    cInputPin m_inputParkTrigger;
-//    cInputPin m_ObjectData;
-
-//    //cOutputPin m_oOutputManipulated;
-
-//public:
-//    cSWE_ParkPilot(const tChar* __info);
-//    virtual ~cSWE_ParkPilot();
-
-//protected: // overwrites cFilter
-//    tResult Init(tInitStage eStage, __exception = NULL);
-//    tResult Start(__exception = NULL);
-//    tResult Stop(__exception = NULL);
-//    tResult Shutdown(tInitStage eStage, __exception = NULL);
-//    tResult OnPinEvent(IPin* pSource, tInt nEventCode, tInt nParam1, tInt nParam2, IMediaSample* pMediaSample);
+#include "stdafx.h"
+#include "math.h"
 
 
+#define OID_ADTF_SWE_PARKPILOT "adtf.swe.parkpilot"
+
+/*!
+* Intersection Point Calculator
+*/
+class cSWE_ParkPilot : public adtf::cFilter
+{
+    ADTF_DECLARE_FILTER_VERSION(OID_ADTF_SWE_PARKPILOT, "SWE ParkPilot", OBJCAT_DataFilter, "ParkPilot", 1, 0,0, "pre alpha version");
+
+    cInputPin m_inputParkTrigger;
+    cInputPin m_ObjectData;
+
+    //cOutputPin m_oOutputManipulated;
+
+public:
+    cSWE_ParkPilot(const tChar* __info);
+    virtual ~cSWE_ParkPilot();
+
+protected: // overwrites cFilter
+    tResult Init(tInitStage eStage, __exception = NULL);
+    tResult Start(__exception = NULL);
+    tResult Stop(__exception = NULL);
+    tResult Shutdown(tInitStage eStage, __exception = NULL);
+    tResult OnPinEvent(IPin* pSource, tInt nEventCode, tInt nParam1, tInt nParam2, IMediaSample* pMediaSample);
 
 
-//private:
-
-//    tFloat32 m_IRFrontRight;
-//    tFloat32 m_IRRearRight;
-
-//    /*! creates all the input Pins*/
-//    tResult CreateInputPins(__exception = NULL);
-//    /*! creates all the output Pins*/
-//    tResult CreateOutputPins(__exception = NULL);
-
-//    // Park functions
-//    tResult searchLot();
 
 
-//    // Media Descriptors
-//    /*! Coder Descriptors for the pins*/
-//    cObjectPtr<IMediaTypeDescription> m_pCoderDesc;
-//    cObjectPtr<IMediaTypeDescription> m_pCoderDescPointsIn;
-//};
+private:
 
-//#endif // _SWE_PARKPILOT_H_
+    tFloat32    m_IRFrontRightCur;
+    tFloat32    m_IRRearRightCur;
+    tFloat32    m_IRFrontRightOld;
+    tFloat32    m_IRRearRightOld;
+    tFloat32    m_distCur;
+    tFloat32    m_distEntry;
+    tFloat32    m_lastIRshort;
+
+    tInt        m_parkTrigger;
+
+    tBool       m_searchActive;
+    tBool       m_entry;
+    tBool       m_entrySaved;
+    tBool       m_minDistReached;
+
+
+    /*! struct containing the odometry input data */
+    typedef struct
+    {
+        tFloat32	 angle;			// angle in radians
+        tFloat32	 distance; 		// distance in mm
+        tFloat32   	 heading;		// heading in radians
+        tFloat32     distanceSum;   // sum of driven distance
+    }odometryData;
+
+    odometryData m_odometryData;
+
+
+
+    /*! creates all the input Pins*/
+    tResult CreateInputPins(__exception = NULL);
+    /*! creates all the output Pins*/
+    tResult CreateOutputPins(__exception = NULL);
+
+    // Park functions
+    tResult searchRoutineAlongside();
+    tResult searchRoutineCross();
+
+    tResult parkRoutineAlongside(tFloat32 lotSize, tFloat32 distStartPark);
+    tResult parkRoutineCross(tFloat32 lotSize, tFloat32 distStartPark);
+
+
+    // Media Descriptors
+    /*! Coder Descriptors for the pins*/
+    cObjectPtr<IMediaTypeDescription> m_pCoderDesc;
+    cObjectPtr<IMediaTypeDescription> m_pCoderDescPointsIn;
+};
+
+#endif // _SWE_PARKPILOT_H_
